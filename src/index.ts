dedf255842c60ecb76c760b3f9eb3da564cad496
Hüseyin Akbaş <huskb.code@gmail.com>
2019-10-12 12:22:03 +0300

    New Version

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
-import { Restless, ContentType, Method } from "./Restless";
+import { Restless} from "./Restless";
 
-/*
-User.get(1)
-User.get(1, {rank: 5})
-User.update(1, {username: "Deneme"})
-*/
 document.addEventListener("DOMContentLoaded", async () => {
-  const R = new Restless("https://jsonplaceholder.typicode.com", ContentType.FORM_URL_ENCODED, {
-    "todos": Method.GET
-  });
-  const User = R.client();
-  console.log(await User.todos(1));
+  const Source = Restless.init("https://jsonplaceholder.typicode.com");
 
-})
+  // $get GET Request
+  const GetResult = await Source.posts.$get(1);
+  const GetResultWithQuery = await Source.posts.$get(1, {limit: 5});
+  
+  // $post GET Request
+  const PostResult = await Source.posts.update.$post(1, {title: "Hello World"});
+  
+  // Source URL generation
+  const Posts = Source.posts.$dup();
+  Posts.update(1, {title: "Hello World"});
+
+});
