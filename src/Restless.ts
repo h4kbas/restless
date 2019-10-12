4d92177f250cd8d1133629511c6500a2bf82f8bb
Hüseyin Akbaş <huskb.code@gmail.com>
2019-10-12 12:22:09 +0300

    New

--- src/Restless.ts
+++ src/Restless.ts
@@ -10,30 +10,46 @@ export enum ContentType{
   FORM_URL_ENCODED = "application/x-www-form-urlencoded"
 }
 
-
-export interface IEndpoints{
-  [ep:string]: Method
-}
-
 export class Restless {
   [x: string]: any;
-  private contentType: ContentType = ContentType.FORM_URL_ENCODED;
-  private url: string | undefined;
-  private eps: IEndpoints = {};
-  
-  constructor(url: string, contenType: ContentType, eps: IEndpoints) {
+  public contentType: ContentType = ContentType.FORM_URL_ENCODED;
+  public url: string | undefined;
+  public seq: string[];
+
+  constructor(url: string) {
     this.url = url;
-    this.contentType = contenType;
-    this.eps = eps;
+    this.seq = [];
   }
 
-  client(){
+  static init(url: string){
+    return new Restless(url).client();
+  }
+
+  public client(){
     return new Proxy(this, {
       get(obj, prop: string) {
-        if( prop in obj.eps){
+        if(prop == "_this") return obj;
+        if(prop == "$get"){
           return async function(...data: any){
-           return await obj.request(prop, obj.eps[prop], data);
-          }   
+            return await obj.request(obj.seq.join("/"), Method.GET, data);
+          }
+        }
+        else if(prop == "$post"){
+          return async function(...data: any){
+            return await obj.request(obj.seq.join("/"), Method.POST, data);
+          }
+        }
+        else if(prop == "$dup"){
+          return function(...data: any){
+            const copy = Restless.init(obj.url + `/${obj.seq.join("/")}`);
+            copy._this.contentType = obj.contentType;
+            obj.seq = [];
+            return copy;
+          }
+        }
+        else{
+          obj.seq.push(prop);
+          return obj.client();
         }
       }
     });
@@ -48,6 +64,7 @@ export class Restless {
     };
     if(method == Method.POST)
       props.body = this.contentType == ContentType.FORM_URL_ENCODED  ? new FormData(data) : JSON.stringify(data);
+    this.seq = [];
     console.log(this.normalize(prop, method, data));
     return (await fetch(this.normalize(prop, method, data), props)).json()
   }
@@ -60,13 +77,13 @@ export class Restless {
       }
       //Situation 2: User.get(param1, {prop}) -> url/get/param1?prop=prop
       if(data[data.length - 1] instanceof Object)
-        return `${this.url}/${prop}/${data.join("/")}?${this.params(data[data.length - 1])}`;
+        return `${this.url}/${prop}/${data.slice(0, -1).join("/")}?${this.params(data[data.length - 1])}`;
       //Situation 3: User.get(param1, param2) -> url/get/param1/param2
       else
-        return `${this.url}/${prop}/${data.join("/")}`;
+        return `${this.url}/${prop ? prop + "/" : ""}${data.join("/")}`;
     }
     else{
-      return `${this.url}/${prop}/${data.join("/")}`;
+      return `${this.url}/${prop ? prop + "/" : ""}${data.join("/")}`;
     }
   }
 
